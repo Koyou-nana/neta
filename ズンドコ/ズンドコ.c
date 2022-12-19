9dd3387b8f3133f57f22481171a13dfe230dab16
ゆききつね <107163439+Koyou-nana@users.noreply.github.com>
2022-12-19 22:28:06 +0900

    ウイルスじゃないよズンドコだよ！！！

--- ズンドコ/ズンドコ.c
+++ ズンドコ/ズンドコ.c
@@ -10,10 +10,11 @@ int result3(int* num);
 
 void main()
 {
-    int temp, i, result2;
+    int temp, i, result2, sleep;
     int result[] = {0, 0, 0, 0, 0};
     char Enter[100];
     char zundoko[100] = "結果：";
+    sleep = 1000;
     printf("乱数取得中...15秒くらいかかります\n");
     for (i = 0; i < 5; i++)
     {
@@ -22,11 +23,13 @@ void main()
         result[i] = temp;
         zundoko2(temp, zundoko);
         printf("%d/5 完了\n", i);
-        Sleep(3000);
+        Sleep(sleep);
+        sleep += 1000;
     }
+    printf("5/5 完了\n");
     result2 = result3(result);
     printf("%s\n", zundoko);
-    if (result2 == 1){
+    if (result2 == 5){
         printf("きよし\n");
         gets(Enter);
     } else {
@@ -49,13 +52,15 @@ char* zundoko2(int num, char* zundoko)
 int result3(int* num)
 {
     int ok[] = {0, 0, 0, 0, 1};
-    int ok2;
-    if (num == ok)
+    int ok2, i, n, m;
+    ok2 = 0;
+    for (i = 0; i < 5; i++)
     {
-        ok2 = 1;
-    } else 
-    {
-        ok2 = 0;
+        n = num[i];
+        m = ok[i];
+        if (n == m){
+            ok2++;
+        }
     }
     return ok2;
 }
